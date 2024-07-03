e678020c0a7b6e477df9a5fecd85bd26b7f47859
Neo <kantapit.sriverapol@gmail.com>
2024-07-03 22:03:04 +0700

    Change name and add constructor

---
+++ Game/AI/Decks/OhmSixSamuraiExecutor.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace WindBot.Game.AI.Decks
+{
+    [Deck("SixSamurai", "AI_OhmSixSamurai")]
+    public class OhmSixSamuraiExecutor : DefaultExecutor
+    {
+        public OhmSixSamuraiExecutor(GameAI ai, Duel duel) 
+            : base(ai, duel)
+        {
+
+        }
+    }
+}
